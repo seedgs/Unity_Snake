f98a023e021d978f6694f81d83030890047423ad
X A <seedgs@qq.com>
2023-01-08 01:12:42 +0800

    20230108_Game_Snake3D
    joystick postion adjustment

--- Assets/script/GamePanel.cs
+++ Assets/script/GamePanel.cs
@@ -5,6 +5,7 @@ using UnityEngine.EventSystems;
 
 public class GamePanel : MonoBehaviour
 {
+    
     public EventTrigger et;
     public RectTransform ImgJoy;
     private PlayerObject player;
@@ -42,11 +43,11 @@ public class GamePanel : MonoBehaviour
         ImgJoy.localPosition = NowPos;
 
         //我们有专门的参数  得到相对于锚点的点，这里的120是指Joy中心点距离外圆的边缘距离 防止摇杆出去
-        if (ImgJoy.anchoredPosition.magnitude > 120)
+        if (ImgJoy.anchoredPosition.magnitude > 70)
         {
             //拉回来
             //单位向量 * 长度 = 临界长度
-            ImgJoy.anchoredPosition = ImgJoy.anchoredPosition.normalized * 120;
+            ImgJoy.anchoredPosition = ImgJoy.anchoredPosition.normalized * 70;
         }
         //让玩家移动
         player.Move(ImgJoy.anchoredPosition);
@@ -54,11 +55,9 @@ public class GamePanel : MonoBehaviour
 
     private void EndOnDrag(BaseEventData data)
     {
-        ImgJoy.anchoredPosition = Vector2.zero;
-
+        ImgJoy.localPosition = new Vector2(-224, -371);
         //停止移动 
         player.Move(Vector2.zero);
     }
 
 }
-
